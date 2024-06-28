1bd8f38dd5e6f1a179a8b5a8e58bf2fb2fcb1fc7
WheatSeeds <georg23kuz@gmail.com>
2024-06-28 15:49:53 +0600

    0.0.9 / added category select

--- src/assets/Components/Store/ProductList.jsx
+++ src/assets/Components/Store/ProductList.jsx
@@ -1,35 +1,65 @@
 import React, {useState} from 'react';
 import data from './storeData.json';
 
-const products = [data.Guitars["Electric-Guitars"], 
-              data.Guitars["Acoustic-Guitars"], 
-              data.Guitars["Bass-Guitars"], 
-              data.Drums, data.Synthesizers]
-
+const Allproducts = {
+  all: [...data.Guitars["Electric-Guitars"], 
+        ...data.Guitars["Acoustic-Guitars"], 
+        ...data.Guitars["Bass-Guitars"], 
+        ...data.Drums, 
+        ...data.Synthesizers],
+  electric: data.Guitars["Electric-Guitars"],
+  acoustic: data.Guitars["Acoustic-Guitars"],
+  bass: data.Guitars["Bass-Guitars"],
+  drums: data.Drums,
+  synths: data.Synthesizers
+};
 
 function ProductList(){
+
+  const [category, setCategory] = useState('all');
+  const products = Allproducts[category];
+
   const[currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 10;
-  const btnClick = (pageNumber) => {
+
+  const btnPagination = (pageNumber) => {
+    if (pageNumber < 1 || pageNumber > Math.ceil(products.length / productsPerPage)) return;
     setCurrentPage(pageNumber);
+  };
+
+  const btnCategorySelect = (newCategory) => {
+    setCategory(newCategory.target.value);
+    setCurrentPage(1); 
   };
 
   return (
     <>
+
+
+      <select className="category-select" onChange={btnCategorySelect}>
+        <option value='all'>All</option>
+        <option value='electric'>Electric Guitars</option>
+        <option value='acoustic'>Acoustic Guitars</option>
+        <option value='bass'>Bass Guitars</option>
+        <option value='drums'>Drums</option>
+        <option value='synths'>Synthesizers</option>
+      </select>
+
       <div className="product-list">
-        {products[0].slice((currentPage - 1) * productsPerPage, currentPage * productsPerPage).map((product) => (
+        {products.slice((currentPage - 1) * productsPerPage, currentPage * productsPerPage).map((product) => (
           <div className="product-card" key={product.name}>
             <img src={product.photo} />
             <p>{product.name}</p>
           </div>
         ))}
       </div>
+
       <div className="pagination">
         <button
-          onClick={() => btnClick(currentPage - 1)}>prev
+          onClick={() => btnPagination(currentPage - 1)}>{"<"}
         </button>
         <button
-          onClick={() => btnClick(currentPage + 1)}>next
+          onClick={() => btnPagination(currentPage + 1)}>{">"}
         </button>
       </div>
     </>
